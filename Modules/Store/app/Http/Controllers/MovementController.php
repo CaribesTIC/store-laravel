c5f726d88b4c8f2556ef85032d582ac06288e69f
Ernesto Canquiz <caribestic123@gmail.com>
2024-10-10 12:36:35 -0400

    feat: (WIP) Receive movement data from frontend and validate it

--- Modules/Store/app/Http/Controllers/MovementController.php
+++ Modules/Store/app/Http/Controllers/MovementController.php
@@ -2,6 +2,8 @@
 
 namespace Modules\Store\Http\Controllers;
 
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\{Request, JsonResponse};
 use Illuminate\Routing\Controller;
@@ -10,13 +12,41 @@ use Modules\Store\Http\Resources\MovementResource;
 /*use Modules\Article\Http\Requests\Article\{
     StoreArticleRequest,
     UpdateArticleRequest
-};
-use Modules\Article\Http\Services\Article\{
-    StoreArticleService,
-    IndexArticleService,
-    UpdateArticleService
 };*/
+use Modules\Store\Http\Services\Movement\{
+    StoreMovementService,
+    //IndexArticleService,
+    //UpdateArticleService
+};
 use Modules\Store\Entities\Movement;
+
+enum MovementTypeId: string
+{
+    case Imput = '1';
+    case Output = '2';
+    case ImputReverses = '3';
+    case OutputReverses = '4';
+
+    public static function isNotValid($value)
+    {
+        return !self::isValid($value);
+    }
+
+    public static function isValid($value)
+    {
+        return in_array($value, self::values());
+    }
+
+    public static function values()
+    {
+        return [self::Imput->value, self::Output->value, self::ImputReverses->value, self::OutputReverses->value];
+    }
+
+    public static function names()
+    {
+        return [self::Imput->name, self::Output->name, self::ImputReverses->name, self::OutputReverses->name];
+    }
+}
 
 class MovementController extends Controller
 {
@@ -79,5 +109,42 @@ class MovementController extends Controller
         return new MovementResource($movement);
     }
 
+    /**
+     * Store a newly created resource in storage.
+     */ 
+    //public function store(StoreArticleRequest $request): JsonResponse
+    public function store(Request $request, string $typeId)
+    {
+        $dataMain = [
+            'type_id' => $typeId,
+            'number' => $request->main["number"],
+            'date_time' => $request->main["date_time"],
+            'subject' => $request->main["subject"],
+            'description' => $request->main["description"],
+            'observation' => $request->main["observation"],
+            'support_type_id' => $request->main["support_type_id"],
+            'support_number' => $request->main["support_number"],
+            'support_date' => '2024-10-10T15:45:00.000Z', //$request->main["support_date"],
+        ];
+
+        $validator = Validator::make(
+            data: $dataMain,
+            rules: [
+                'type_id' => [Rule::enum(MovementTypeId::class)],
+                'number' => ['required', 'numeric'],
+                'date_time' => ['required', 'date'],
+                'subject' => ['required', 'string'],
+                'support_type_id' => ['required', 'string'],
+                'support_number' => ['required', 'string'],
+                'support_date' => ['required', 'date'],
+            ]
+        );
+
+        return ($validator->fails()) ? "true " : "false" ;
+
+    }
+
 
 }
+
+
